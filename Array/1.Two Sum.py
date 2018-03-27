016d317ab208eac9ed242c50eaa11026071028ff
gucang <33020361+gucang@users.noreply.github.com>
2018-03-27 17:38:09 +0800

    Update and rename 1.Two Sum to 1.Two Sum.py

--- Array/1.Two Sum.py
+++ Array/1.Two Sum.py
@@ -5,6 +5,7 @@
 #Because nums[0] + nums[1] = 2 + 7 = 9
 #return [0, 1]
 
+#自己
 class Solution:
     def twoSum(self, nums, target):
         """
@@ -15,6 +16,19 @@ class Solution:
         for i in range(len(nums)):
             if (target-nums[i]) in nums:
                 j=nums.index(target-nums[i])
-                if i!=j:
+                if i!=j:     #通过索引判断是否重复
                     return [i,j]
-
+                    
+#其他                 
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dic = {}  #hash table
+        for index, num in enumerate(nums):
+            if num in dic:
+                return [dic[num], index]
+            dic[target - num] = index
