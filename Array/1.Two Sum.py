98be08eca25f81991b3006915a39d737496f4676
gucang <33020361+gucang@users.noreply.github.com>
2018-03-31 15:22:58 +0800

    Update 1.Two Sum.py

--- Array/1.Two Sum.py
+++ Array/1.Two Sum.py
@@ -32,3 +32,7 @@ class Solution:
             if num in dic:
                 return [dic[num], index]
             dic[target - num] = index
+
+#错误点：没有判断如果list中有重复的情况
+
+#思路： Ⅰ遍历+索引([].index())； Ⅱ索引和值可以考虑到hash表+key和value结构；让key就是要找的b，让value记录a的index，也就是结果需要的索引(他人)
